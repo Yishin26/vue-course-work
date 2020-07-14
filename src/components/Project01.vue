15f18822de360b5d13ebb88e845790ae539e4e35
Yishin26 <yishinlin@outlook.com>
2020-07-14 23:02:17 +0800

    Setup the Project01

---
+++ src/components/Project01.vue
@@ -0,0 +1,57 @@
+
+<template>
+  <div class='project01'>
+     <section class="row">
+        <div class="small-6 columns">
+            <h1 class="text-center">YOU</h1>
+            <div class="healthbar">
+                <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
+
+                </div>
+            </div>
+        </div>
+        <div class="small-6 columns">
+            <h1 class="text-center">MONSTER</h1>
+            <div class="healthbar">
+                <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="row controls">
+        <div class="small-12 columns">
+            <button id="start-game">START NEW GAME</button>
+        </div>
+    </section>
+    <section class="row controls">
+        <div class="small-12 columns">
+            <button id="attack">ATTACK</button>
+            <button id="special-attack">SPECIAL ATTACK</button>
+            <button id="heal">HEAL</button>
+            <button id="give-up">GIVE UP</button>
+        </div>
+    </section>
+    <section class="row log">
+        <div class="small-12 columns">
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Project01'
+}
+</script>
+
+<style>
+
+@import '../css/foundation.min.css';
+@import '../css/app.css';
+</style>
